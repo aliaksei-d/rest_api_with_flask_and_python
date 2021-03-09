9a48b21bdb31a60b15e29f616f964e3e1487df1b
alexey-dob <alexey.doborovich@blazemeter.com>
2021-03-09 22:32:06 +0300

    project_created

---
+++ code.py
@@ -0,0 +1,14 @@
+
+students_list = {
+    'student1': {'name': 'Nikola', 'school': 'Paris', 'grades': (44, 55, 33)},
+    'student2': {'name': 'Alex', 'school': 'Minsk', 'grades': (1, 2, 3)},
+    'student3': {'name': 'Sam', 'school': 'NY', 'grades': (22, 23, 24)}
+}
+
+print(students_list.values())
+
+# def average_grade_all_students(students_list):
+#     for student in students_list:
+#         print(student['grades'])
+#
+# print(average_grade_all_students(students_list))
